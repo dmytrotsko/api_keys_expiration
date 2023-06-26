f445154da6948a40c44953eab4bcd7ec923f0b4c
Dmytro Trotsko <dmytrotsko@gmail.com>
2023-06-26 20:54:50 +0300

    Removed utils.py, changed update_last_usage.py script

--- update_last_usage.py
+++ update_last_usage.py
@@ -1,64 +1,57 @@
-import argparse
+import os
+from datetime import datetime as dtime
 
-from utils import get_redis_instance, get_cnx_cur, close_cnx
+import delphi.operations.secrets as secrets
+import mysql.connector
+import redis
 
-# REDIS_HOST = "delphi_redis_instance"
-# REDIS_PORT = 6379
-# REDIS_DB_INDEX = 0
-# REDIS_KEY_PATTERN = "*LAST_USAGE*"
-
-# DB_USER = "user"
-# DB_PASS = "pass"
-# DB_HOST = "0.0.0.0"
-# DB_PORT = 13306
-# DB_NAME = "epidata"
+REDIS_HOST = os.environ.get("REDIS_HOST", "delphi_redis")
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD", "1234")
+LAST_USED_KEY_PATTERN = "*LAST_USED*"
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(
-        prog="Update api_user last_time_used column",
-        description="Update api_user last_time_used based on records in Redis"
-    )
-
-    parser.add_argument("-rh", "--redis_host", help="Redis host")
-    parser.add_argument("-rp", "--redis_port", help="Redis port")
-    parser.add_argument("-rdi", "--redis_db_index", help="Redis DB index")
-    parser.add_argument("-rkp", "--redis_key_pattern", help="Redis Key pattern")
-
-    parser.add_argument("-du", "--db_user", help="DB user")
-    parser.add_argument("-dup", "--db_user_password", help="DB user password")
-    parser.add_argument("-dh", "--db_host", help="DB host")
-    parser.add_argument("-dp", "--db_port", help="DB port")
-    parser.add_argument("-dn", "--db_name", help="DB name")
-    args = parser.parse_args()
-    return args
-
-
-def get_key_val_pairs(redis_cli, pattern: str):
+def redis_get_last_time_used(redis_cli, pattern: str):
     keys = redis_cli.keys(pattern=pattern)
-    key_val_pairs = {key.split("/")[1]: redis_cli.get(key) for key in keys}
+    key_val_pairs = {
+        str(key).split("/")[1]: dtime.strptime(str(redis_cli.get(key)), "%Y-%m-%d").date() for key in keys
+    }
     return key_val_pairs
 
 
-def update_database(api_key: str, last_time_used: str, cur):
+def db_get_last_time_used(api_key: str, cur):
     cur.execute(
         f"""
-        UPDATE
-            api_user
-        SET last_time_used = "{last_time_used}"
+        SELECT
+            last_time_used
+        FROM api_user
         WHERE api_key = "{api_key}"
-    """
+        LIMIT 1
+        """
     )
+    last_time_used = cur.fetchone()
+    return last_time_used[0]
 
 
 def main():
-    args = parse_args()
-    redis_cli = get_redis_instance(host=args.redis_host, port=args.redis_port, db_index=args.redis_db_index)
-    key_val_pairs = get_key_val_pairs(redis_cli, args.redis_key_pattern)
-    cnx, cur = get_cnx_cur(db_user=args.db_user, db_user_password=args.db_user_password, db_host=args.db_host, db_name=args.db_name, db_port=args.db_port)
-    for k, v in key_val_pairs.items():
-        update_database(k, v, cur)
-    close_cnx(cnx)
+    redis_cli = redis.Redis(host=REDIS_HOST, password=REDIS_PASSWORD, decode_responses=True)
+    key_val_pairs = redis_get_last_time_used(redis_cli, LAST_USED_KEY_PATTERN)
+    u, p = secrets.db.epi
+    cnx = mysql.connector.connect(database="epidata", user=u, password=p, host=secrets.db.host)
+    cur = cnx.cursor()
+    for api_key, last_time_used in key_val_pairs.items():
+        db_last_time_used = db_get_last_time_used(api_key, cur)
+        if db_last_time_used == None or last_time_used > db_last_time_used:
+            cur.execute(
+                f"""
+                UPDATE
+                    api_user
+                SET last_time_used = "{last_time_used}"
+                WHERE api_key = "{api_key}"
+            """
+            )
+    cur.close()
+    cnx.commit()
+    cnx.close()
 
 
 if __name__ == "__main__":
